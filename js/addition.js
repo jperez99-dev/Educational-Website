f142463d088df7c614a3364e1203a75079da2277
Jordan Perez <jordan.perez01@gmail.com>
2021-05-02 21:42:16 -0500

    Added more Js files and standard logic
    Added basic logic to quiz js files and added more styles

---
+++ js/addition.js
@@ -0,0 +1,4 @@
+var body = document.getElementsByTagName("main");
+var ptag = document.createElement("p");
+ptag.textContent = "something";
+document.append(ptag);
---
+++ js/multiplication.js
@@ -0,0 +1,36 @@
+var ranNumber1;
+var ranNumber2;
+var score = 0;
+var userInput;
+var questionCount = 1;
+
+
+// loop through each question
+for(i = 1; i < 11; i++){
+// generate random numbers to solve from
+ranNumber1 = Math.floor(Math.random() * 100);
+ranNumber2 = Math.floor(Math.random() * 100);
+questionCount = [i];
+
+// Get random numbers sum
+var answer = ranNumber1 + ranNumber2;
+ 
+
+// Get user input
+var userInput = prompt(ranNumber1 + "+" + ranNumber2 + "= ?");
+
+
+if (userInput == answer){
+    score +=10;
+    alert("Correct!");
+    alert(score);
+} else {
+    alert("wrong");
+    alert(score);
+}
+
+alert("question " + questionCount);
+}
+
+
+
